f31ae7d47649923a1221707f2763bc9691f353ef
akzin05 <aungkhantzin50@gmail.com>
2020-06-13 03:20:19 +0800

    commmit 1

---
+++ MVCStoredProducreSample/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace MVCStoredProducreSample.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MVCStoredProducreSample.Models.MovieDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "MVCStoredProducreSample.Models.MovieDbContext";
+        }
+
+        protected override void Seed(MVCStoredProducreSample.Models.MovieDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
